12e6fb6d3cb667d0a335ed0128eb39ee12c40503
kema-dev <github@kema.dev>
2025-05-11 14:04:37 +0200

    up

--- internal/pkg/host/host.go
+++ internal/pkg/host/host.go
@@ -9,12 +9,13 @@ import (
 type (
 	// A base host, used to construct URLs
 	Host url.URL
-	// An URL template used to distribute traffic
+	// An URL template, used to distribute traffic
 	URL struct {
 		// Host where traffic is destinated
 		BaseHost Host
 		// Path pattern used for `http.ServeMux`, see https://pkg.go.dev/net/http#ServeMux
-		// Please note that `HOST` part won't be used, host matching is done via `BaseHost` in Gateway API components
+		// Matching should be done in Gateway API components, replacing templates values with,
+		// application-specific values, proxying to application that will match using path template
 		PathPattern string
 	}
 )
@@ -34,67 +35,91 @@ func (u URL) String() string {
 const (
 	// HTTPs protocol scheme
 	SchemeHTTPS string = "https"
+	// `http.ServeMux` matching pattern every application / service should use
+	// `entity` is service / application's name
+	// `version` is service / application's SemVer version (e.g. `v0.1.12`)
+	APIPathPattern string = "{entity}/{version}/"
 )
 
 var (
 	// Base host for internet-facing public applications
-	HostPublicInternetFacingApp Host = Host(url.URL{
+	BaseHostPublicInternetFacingApp Host = Host(url.URL{
 		Scheme: SchemeHTTPS,
 		Host:   domain.DomainKemaDotDev.String(),
 	})
 	// Base host for internet-facing public services
-	HostPublicInternetFacingService Host = Host(url.URL{
+	BaseHostPublicInternetFacingService Host = Host(url.URL{
 		Scheme: SchemeHTTPS,
 		Host:   domain.DomainKemaDotCloud.String(),
 	})
 	// Base host for internet-facing internal services
-	HostInternalInternetFacingService Host = Host(url.URL{
+	BaseHostInternalInternetFacingService Host = Host(url.URL{
 		Scheme: SchemeHTTPS,
 		Host:   domain.DomainKemaDotRun.String(),
 	})
 	// Base host for non-internet internal services
-	HostInternalPrivateService Host = Host(url.URL{
+	BaseHostInternalPrivateService Host = Host(url.URL{
 		Scheme: SchemeHTTPS,
 		Host:   domain.DomainKemaDotInternal.String(),
 	})
+	// Base host for preview applications
+	BaseHostPrivatePreview Host = Host(url.URL{
+		Scheme: SchemeHTTPS,
+		Host:   "preview" + BaseHostInternalPrivateService.Host,
+	})
 
+	// Host for company's VCS service
+	HostVCS Host = Host(url.URL{
+		Scheme: SchemeHTTPS,
+		Host:   "vcs." + BaseHostInternalInternetFacingService.Host,
+	})
+	// Host for company's chat service
+	HostChat Host = Host(url.URL{
+		Scheme: SchemeHTTPS,
+		Host:   "chat." + BaseHostInternalInternetFacingService.Host,
+	})
 	// Host for company's main website
 	HostMainWebsite Host = Host(url.URL{
 		Scheme: SchemeHTTPS,
 		Host:   "www." + domain.DomainKemaDotDev.String(),
+	})
+	// Host for company's forum
+	HostForum Host = Host(url.URL{
+		Scheme: SchemeHTTPS,
+		Host:   "discuss." + domain.DomainKemaDotDev.String(),
+	})
+	// Host for Kubernetes control planes (kube-apiserver access)
+	HostKubeControlePlane Host = Host(url.URL{
+		Scheme: SchemeHTTPS,
+		Host:   "kube.{clusterName}." + BaseHostInternalPrivateService.Host + ":6443",
+	})
+	// Host for Kubernetes control planes (kube-apiserver access)
+	HostReviewApp Host = Host(url.URL{
+		Scheme: SchemeHTTPS,
+		// `repoFQDN` is repository's Fully Qualified Domain Name, i.e. go module name, in lowercase alphanum + dashes form (e.g. `host-tld-owner-repo`)
+		Host: "{repoFQDN}-{prNumber}." + BaseHostPrivatePreview.Host,
+	})
+	// Host used to access consoles of internal services
+	HostServiceConsole Host = Host(url.URL{
+		Scheme: SchemeHTTPS,
+		// `service` is service's name
+		Host: "{service}." + BaseHostInternalPrivateService.Host,
 	})
 	// Host for company's main API
 	HostMainApi Host = Host(url.URL{
 		Scheme: SchemeHTTPS,
 		Host:   "api." + domain.DomainKemaDotInternal.String(),
 	})
+
+	// Base URL for all APIs, providing a common structure for all applications / services
+	// All applications / services should use the same pattern
 	URLMainApi URL = URL{
 		BaseHost:    HostMainApi,
-		PathPattern: "{service}/v{version}/",
+		PathPattern: APIPathPattern,
+	}
+	// URL for company's security guidelines & responsible disclosure procedure
+	URLSecurityGuidelines URL = URL{
+		BaseHost:    HostForum,
+		PathPattern: "/c/security",
 	}
 )
-
-// ## Domains definition
-
-// - Internet-facing public production - `kema.dev`
-//   - Main website - `www.kema.dev`
-//   - API host - `api.kema.dev`
-//     - API routes - `api.kema.dev/<service>/v<version>/*`
-//       - Microservice name - `service`
-//       - Microservice API version - `version`
-// - Internet-facing public services - `kema.cloud`
-//   - Main website - `www.kema.cloud`
-//   - Forum - `discuss.kema.cloud`
-//     - Security guidelines & responsible disclosure procedure - `https://discuss.kema.cloud/c/security`
-// - `kema.run` - Internet-facing internal services
-//   - VCS - `vcs.kema.run`
-//   - Internal chat & VOIP - `chat.kema.run`
-// - Private internal - `kema.internal`
-//   - Kubernetes control planes nodes VIP - `kube.<cluster name>.kema.internal`
-//   - Review applications - `preview.kema.internal`
-//     - Review applications - `<repo>-<pr number>.preview.kema.internal`
-//       - Repository FQDN holding application code - `<repo>`
-//       - PR number that triggered deployment - `<pr number>`
-//   - Internal services / administration consoles - `<service>.kema.internal`
-//     - Service name - `service`
-// - Internet-facing personal - `doublej.fr`
