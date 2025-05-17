3f577cb4c89a5842f7022042ec1015dec062eacc
kema-dev <github@kema.dev>
2025-05-17 11:51:42 +0200

    up

--- pkg/k8s/priorityclass/priorityclass.go
+++ pkg/k8s/priorityclass/priorityclass.go
@@ -11,11 +11,48 @@ import (
 func CreateDefaultPriorityClasses(ctx *pulumi.Context) error {
 	_, err := schedulingv1.NewPriorityClass(
 		ctx,
+		"eventual",
+		&schedulingv1.PriorityClassArgs{
+			Value: pulumi.Int(-1000000),
+			Description: pulumi.String(
+				"Low priority class, lower than low, default, normal or high, do not preempts other pods",
+			),
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:   pulumi.String("eventual"),
+				Labels: pulumi.StringMap{},
+			},
+			PreemptionPolicy: pulumi.String("Never"),
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create eventual priority class: %v", err)
+	}
+
+	_, err = schedulingv1.NewPriorityClass(
+		ctx,
+		"low",
+		&schedulingv1.PriorityClassArgs{
+			Value: pulumi.Int(-1000),
+			Description: pulumi.String(
+				"Low priority class, lower than default, normal or high, preempts other pods",
+			),
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:   pulumi.String("low"),
+				Labels: pulumi.StringMap{},
+			},
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create low priority class: %v", err)
+	}
+
+	_, err = schedulingv1.NewPriorityClass(
+		ctx,
 		"default",
 		&schedulingv1.PriorityClassArgs{
 			Value: pulumi.Int(0),
 			Description: pulumi.String(
-				"Default priority class, used by pods that do not specify a priority class",
+				"Default priority class, lower than normal or high, to be used by pods that do not specify a priority class",
 			),
 			GlobalDefault: pulumi.Bool(true),
 			Metadata: &metav1.ObjectMetaArgs{
@@ -30,23 +67,39 @@ func CreateDefaultPriorityClasses(ctx *pulumi.Context) error {
 
 	_, err = schedulingv1.NewPriorityClass(
 		ctx,
-		"default",
+		"normal",
 		&schedulingv1.PriorityClassArgs{
-			Value: pulumi.Int(0),
+			Value: pulumi.Int(1000),
 			Description: pulumi.String(
-				"Default priority class, used by pods that do not specify a priority class",
+				"Normal priority, higher than default, preempts other pods",
 			),
-			GlobalDefault: pulumi.Bool(true),
 			Metadata: &metav1.ObjectMetaArgs{
-				Name:   pulumi.String("default"),
+				Name:   pulumi.String("normal"),
 				Labels: pulumi.StringMap{},
 			},
 		},
 	)
 	if err != nil {
-		return fmt.Errorf("failed to create default priority class: %v", err)
+		return fmt.Errorf("failed to create normal priority class: %v", err)
 	}
+
+	_, err = schedulingv1.NewPriorityClass(
+		ctx,
+		"high",
+		&schedulingv1.PriorityClassArgs{
+			Value: pulumi.Int(1000000),
+			Description: pulumi.String(
+				"High priority, higher normal, preempts other pods",
+			),
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:   pulumi.String("high"),
+				Labels: pulumi.StringMap{},
+			},
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create high priority class: %v", err)
+	}
+
 	return nil
 }
-
-// -2147483648 to 1000000000
