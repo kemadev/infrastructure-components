62745cfc34bbbed6fe0648458af959c667265189
Jérémy Jourdan <github@kema.dev>
2025-07-11 14:17:35 +0200

    up

--- pkg/github/repo/rulesets.go
+++ pkg/github/repo/rulesets.go
@@ -16,9 +16,10 @@ type RulesetsArgs struct {
 var RulesetsDefaultArgs = RulesetsArgs{
 	RequiredReviewersMain: 1,
 	RequiredStatusChecks: []string{
-		"Global - CI / Scan code",
-		"Global - CI / Scan secrets",
-		"Check PR title / Check PR title",
+		"Global - CI / Secrets scan",
+		"Global - CI / Dependencies scan",
+		"Global - CI / Static Application Security Testing",
+		"PR - Title Check / PR title check",
 	},
 }
 
